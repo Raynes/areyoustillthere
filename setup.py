dd25d9928daa12a41896f48041e4892380c86ce5
Anthony Grimes <i@raynes.me>
2014-05-26 16:56:40 -0700

    Actual useful setup.py.

--- setup.py
+++ setup.py
@@ -1,15 +1,19 @@
+"""Are You Still There is a simple Python library testing if
+email addresses actually exist.
+
+"""
 from setuptools import setup
 
 setup(
     name='areyoustillthere',
-    version='1.0',
+    version='0.1.0',
     long_description=__doc__,
-    packages=['areyoustillthere'],
+    packages=['ayst'],
     include_package_data=True,
     author='Anthony Grimes',
+    author_email='i@raynes.me',
+    url='https://github.com/Raynes/areyoustillthere',
     description='Find dem emails yo.',
     license='MIT',
-    install_requires=['requests>=2.2.1', 'nameparser>=0.2.9',
-                      'pydns>=2.3.6', 'gevent>=1.0.1',
-                      'rapportive'],
+    install_requires=['pydns>=2.3.6']
 )
