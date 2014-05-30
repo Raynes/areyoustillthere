ba33399f59abcf5ab64e437d7291e2e350f74389
Anthony Grimes <i@raynes.me>
2014-05-30 09:42:30 -0700

    Inc version.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 setup(
     name='areyoustillthere',
-    version='0.1.0',
+    version='0.1.1',
     long_description=__doc__,
     packages=['ayst'],
     include_package_data=True,
